f8b902a1731f37eeb26d3ed0089b72e8d866eba7
David Tan <davified@gmail.com>
2016-10-17 16:43:17 +0800

    added hero detail

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-
 import { AppComponent }  from './app.component';
-import { HomepageComponent }  from './homepage/homepage.component';
-
+import { HeroDetailComponent } from './detail/hero-detail.component';
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
